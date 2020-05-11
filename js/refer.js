94c7cb96b40ee06da3f0f1191703bb4419075d70
vtrev <baloyi.trev@gmail.com>
2020-05-11 03:24:37 +0200

    removed all font link rel and import from css

--- js/refer.js
+++ js/refer.js
@@ -1,23 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var dots = document.querySelector("#dots");
-    var moreText = document.querySelector("#more");
-    var btnText = document.querySelector(".btn-white");
+    var moreText = document.querySelector("#more-refer-text");
+    var readMoreButton = document.querySelector("#more-refer-btn");
 
-    //define a function called readMore 
-    btnText.addEventListener('click', () => {
-        if (dots.style.display === "none") {
-            dots.style.display = "inline-block";
-            btnText.innerHTML = "Read more";
-            moreText.style.display = "none";
-        } else {
-            dots.style.display = "none";
-            btnText.innerHTML = "Read less";
+
+    readMoreButton.addEventListener('click', () => {
+        if (readMoreButton.innerHTML === "Read more here") {
             moreText.style.display = "inline-block";
+            readMoreButton.innerHTML = "Read less";
+        } else {
+            readMoreButton.innerHTML = "Read more here";
+            moreText.style.display = "none";
+
         }
     })
+    
     // //parallax
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, {});
-    //carousel
 
 })
