7baed03d7bfafa9020421b6b9619c0fb34104ff9
vtrev <baloyi.trev@gmail.com>
2020-05-11 12:43:45 +0200

    removed incentive plan tables && fixed more text

--- js/incentiveplan.js
+++ js/incentiveplan.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
+    let moreZoomText = document.querySelector("#more-incentives-text");
+    let moreZoomButton = document.querySelector("#readmore-incentives-btn");
+
+    moreZoomButton.addEventListener('click', (button) => {
+        if (moreZoomButton.innerHTML === "Read more here") {
+            moreZoomText.style.display = "inline-block";
+            moreZoomButton.innerHTML = "Read less";
+        } else {
+            moreZoomButton.innerHTML = "Read more here";
+            moreZoomText.style.display = "none";
+
+        }
+    })
+
     // //parallax
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, {});
